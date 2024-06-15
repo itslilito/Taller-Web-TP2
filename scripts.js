c0faab3060eb5304c7557d9a5384c0fc7dbd75f0
NicanorGarciaDM <nicanorgarcia299@gmail.com>
2024-06-15 04:35:56 -0300

    Header (navbar) con display en noticias
    me rompi el culo con esto. tambien le agregué la search bar (creo que no habia actualizado cuando la agregué)

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/* HOVER SHOW/HIDE CON EL BOTON DE NOTICIAS */ 
+
+document.getElementById('boton-noticias').addEventListener('mouseenter', function() {
+    document.querySelector('.noticias-hover').classList.add('show');
+});
+
+document.getElementById('id-noticias-hover').addEventListener('mouseenter', function() {
+    document.querySelector('.noticias-hover').classList.add('show');
+});
+
+document.getElementById('boton-noticias').addEventListener('mouseleave', function() {
+    document.querySelector('.noticias-hover').classList.remove('show');
+});
+
+document.getElementById('id-noticias-hover').addEventListener('mouseleave', function() {
+    document.querySelector('.noticias-hover').classList.remove('show');
+});
+
+/* otro script */
