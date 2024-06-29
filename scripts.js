a4dd278677d4832ea800d1c1509038a8324a2994
NicanorGarciaDM <nicanorgarcia299@gmail.com>
2024-06-29 19:47:23 -0300

    hover de notis evaporado

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-// HOVER SHOW/HIDE CON EL BOTON DE NOTICIAS //
-
-document.getElementById('boton-noticias').addEventListener('mouseenter', function() {
-    document.querySelector('.noticias-hover').classList.add('show');
-});
-
-document.getElementById('id-noticias-hover').addEventListener('mouseenter', function() {
-    document.querySelector('.noticias-hover').classList.add('show');
-});
-
-document.getElementById('boton-noticias').addEventListener('mouseleave', function() {
-    document.querySelector('.noticias-hover').classList.remove('show');
-});
-
-document.getElementById('id-noticias-hover').addEventListener('mouseleave', function() {
-    document.querySelector('.noticias-hover').classList.remove('show');
-});
-
 // HOVER DE ICONO NEGRO A BLANCO //
 
 document.getElementById('boton-busqueda').addEventListener('mouseenter', function() {
