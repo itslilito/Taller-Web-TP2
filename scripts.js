ec5bf32e631ae69fa6fdff48b16163bd27f08676
NicanorGarciaDM <nicanorgarcia299@gmail.com>
2024-06-29 19:45:19 -0300

    actualizado a artistas
    falta estilizar, ejemplos y funcionalidad

--- scripts.js
+++ scripts.js
@@ -15,3 +15,13 @@ document.getElementById('boton-noticias').addEventListener('mouseleave', functio
 document.getElementById('id-noticias-hover').addEventListener('mouseleave', function() {
     document.querySelector('.noticias-hover').classList.remove('show');
 });
+
+// HOVER DE ICONO NEGRO A BLANCO //
+
+document.getElementById('boton-busqueda').addEventListener('mouseenter', function() {
+    document.querySelector('.imagen-boton-busqueda').classList.add('blanco');
+});
+
+document.getElementById('boton-busqueda').addEventListener('mouseleave', function() {
+    document.querySelector('.imagen-boton-busqueda').classList.remove('blanco');
+});
